bbbe4ba9c82263d74ffeb044a003d19eaace7906
nicholasohjj <nicholasohjj@gmail.com>
2023-12-26 23:32:57 +0800

    modified vercel.json

--- controllers/markersController.js
+++ controllers/markersController.js
@@ -2,15 +2,17 @@ const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const Joi = require("joi");
 const { v4: uuidv4 } = require("uuid");
 const {
-    DynamoDBDocumentClient,
-    ScanCommand,
-    GetCommand,
-    PutCommand,
-    UpdateCommand,
-    DeleteCommand,
-    QueryCommand,
-  } = require("@aws-sdk/lib-dynamodb");
-  
+  DynamoDBDocumentClient,
+  ScanCommand,
+  GetCommand,
+  PutCommand,
+  UpdateCommand,
+  DeleteCommand,
+  QueryCommand,
+} = require("@aws-sdk/lib-dynamodb");
+
+const redis = require("redis");
+
 const client = new DynamoDBClient({
   region: process.env.AWS_REGION,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -19,14 +21,17 @@ const client = new DynamoDBClient({
 const docClient = DynamoDBDocumentClient.from(client);
 
 const markerSchema = Joi.object({
-    coordinate: Joi.object({
-      latitude: Joi.number().required(),
-      longitude: Joi.number().required()
-    }).required(),
-    title: Joi.string().required(),
-    description: Joi.string().required(),
-    image: Joi.string().uri().required()
-  });
+  coordinate: Joi.object({
+    latitude: Joi.number().required(),
+    longitude: Joi.number().required(),
+  }).required(),
+  title: Joi.string().required(),
+  description: Joi.string().required(),
+  image: Joi.string().uri().required(),
+});
+
+let redisClient;
+
 
 const getMarkers = async (req, res) => {
   const command = new ScanCommand({
@@ -44,37 +49,37 @@ const getMarkers = async (req, res) => {
 };
 
 const addMarker = async (req, res) => {
-    const { error, value } = markerSchema.validate(req.body);
-    if (error) {
-      console.error("Invalid marker:", error);
-      return res.status(400).json({ error: "Invalid marker" });
-    }
+  const { error, value } = markerSchema.validate(req.body);
+  if (error) {
+    console.error("Invalid marker:", error);
+    return res.status(400).json({ error: "Invalid marker" });
+  }
 
-    const { coordinate, title, description, image } = value;
-    const id = uuidv4();
+  const { coordinate, title, description, image } = value;
+  const id = uuidv4();
 
-    const command = new PutCommand({
-      TableName: "markers",
-      Item: {
-        id,
-        coordinate,
-        title,
-        description,
-        image,
-      },
-    });
+  const command = new PutCommand({
+    TableName: "markers",
+    Item: {
+      id,
+      coordinate,
+      title,
+      description,
+      image,
+    },
+  });
 
-    try {
-      const response = await docClient.send(command);
-      console.log("Add marker succeeded.", response);
-      res.json({ id });
-    } catch (err) {
-      console.error("Unable to add marker. Error:", JSON.stringify(err, null, 2));
-      res.status(500).json(err);
-    }
-}
+  try {
+    const response = await docClient.send(command);
+    console.log("Add marker succeeded.", response);
+    res.json({ id });
+  } catch (err) {
+    console.error("Unable to add marker. Error:", JSON.stringify(err, null, 2));
+    res.status(500).json(err);
+  }
+};
 
 module.exports = {
   getMarkers,
-  addMarker
+  addMarker,
 };
