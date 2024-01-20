5af4fedb9e00b6dec1684b1bddf39840114888a2
Restyled.io <commits@restyled.io>
2024-01-20 12:23:05 +0000

    Restyled by prettier

--- app.js
+++ app.js
@@ -11,13 +11,15 @@ const canteensRoutes = require("./routes/canteensRoutes");
 app.use(cors());
 app.use(express.json());
 app.use(
-    rateLimit({
-      windowMs : 15 * 60 * 1000, // 15 minutes
-      max : 100,                 // limit each IP to 100 requests per windowMs
-    }),
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  }),
 );
 // Use routes
-app.get("/", (req, res) => { res.json("Hello World!"); });
+app.get("/", (req, res) => {
+  res.json("Hello World!");
+});
 
 app.use("/markers", markersRoutes);
 app.use("/items", itemsRoutes);
