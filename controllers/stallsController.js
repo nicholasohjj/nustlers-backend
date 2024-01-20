88c363eecccb8f19e02c1214bc6ea90629786f7f
Restyled.io <commits@restyled.io>
2024-01-20 12:26:52 +0000

    Restyled by clang-format

--- controllers/stallsController.js
+++ controllers/stallsController.js
@@ -1,13 +1,13 @@
 const Joi = require("joi");
-const { v4: uuidv4 } = require("uuid");
+const {v4 : uuidv4} = require("uuid");
 const supabase = require("../supabase/supabase");
 
 const stallSchema = Joi.object({
-  stall_id: Joi.string().required(),
-  stall_name: Joi.string().required(),
-  stall_cuisine: Joi.string().required(),
-  stall_image: Joi.string().uri().required(),
-  items_ids: Joi.number().positive().required(),
+  stall_id : Joi.string().required(),
+  stall_name : Joi.string().required(),
+  stall_cuisine : Joi.string().required(),
+  stall_image : Joi.string().uri().required(),
+  items_ids : Joi.number().positive().required(),
 });
 
 // Trial get info
@@ -21,15 +21,15 @@ const stallSchema = Joi.object({
 // }
 
 const getStalls = async (req, res) => {
-  const { data, error } = await supabase.from("stalls").select("*");
+  const {data, error} = await supabase.from("stalls").select("*");
 
   res.json(data);
 };
 
 const addStall = async (stall) => {
   // try{
-    const { value, error } = stallSchema.validate(stall);
-    console.log("This is the Request body", stall);
+  const {value, error} = stallSchema.validate(stall);
+  console.log("This is the Request body", stall);
   // }
   if (error) {
     console.log("Error:", error);
@@ -42,7 +42,7 @@ const addStall = async (stall) => {
   // Insert new information
   console.log("This is the validated body values", value);
 
-  const { data, errors } = await supabase.from("stalls").insert(value);
+  const {data, errors} = await supabase.from("stalls").insert(value);
 };
 
 const addStalls = async (req, res) => {
@@ -51,7 +51,7 @@ const addStalls = async (req, res) => {
     const addStallPromises = stalls.map((stall) => addStall(stall));
     Promise.all(addStallPromises);
   } catch {
-      return res.status(400).json({ error: "Invalid stall" });
+    return res.status(400).json({error : "Invalid stall"});
   }
 };
 
