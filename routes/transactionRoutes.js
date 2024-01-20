a479668299efc989a5fad0446cc4478f36a8ef3d
nictjh <nicholastok0101@gmail.com>
2024-01-20 18:52:55 +0800

    added updateTrans and delTrans

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -5,5 +5,8 @@ const transactionsController = require('../controllers/transactionsController');
 router.get('/', transactionsController.getTransactions)
 router.post('/add', transactionsController.addTransaction)
 router.post('/adds', transactionsController.addTransactions)
+router.put("/:transaction_id", transactionsController.updateTransactions)
+router.delete("/:transaction_id", transactionsController.delTransaction)
+
 
 module.exports = router;
